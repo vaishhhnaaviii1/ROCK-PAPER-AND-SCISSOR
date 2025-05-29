9b700c3629b941b26bc9d395af434ad369e9f445
Vaishnavi <vaishhhnaviii2@gmail.com>
2025-05-29 23:01:52 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,14 @@
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll(".choice");
+
+const platGame = (userchoice) => {}
+
+choices.forEach((choice) => {
+  console.log(choice);
+  choice.addEventListener("click", () =>{
+    const userchoice=choice.getAttribute("id");
+    console.log("choice was clicked", userchoice);
+  })
+})
